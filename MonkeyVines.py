0680bc19961373ecbc0072240eec4bb86b1d45b6
ayush <aks010598@gmail.com>
2021-01-21 13:35:41 +0530

    MonkeyVines Code updated..

--- MonkeyVines.py
+++ MonkeyVines.py
@@ -1,11 +1,24 @@
 # Input:
 # 3
 # []
+#
 # [[][[]]]
 #
 # Output:
 # 1 2
 # 2 1
 # 3 8
+
+for _ in range(int(input())):
+    s=input()
+    cnt=0
+    for i in s:
+        if i=="[":
+            cnt+=1
+    if cnt==0:
+        print(_+1,1)
+    else:
+        print(_+1,cnt*2)
+
 
 
